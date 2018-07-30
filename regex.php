3e8d5ea54051b3cf3b6623aa7cfaf1cb9d9dfc13
Julien Laville <j.laville@claravista.dom>
2018-07-30 18:01:37 +0200

    add link to embed, filter

--- regex.php
+++ regex.php
@@ -3,7 +3,7 @@
 class Regex
 {
 	/* Change the link to an embed version */
-    public function linkToEmbed( $string )
+    public function linkToEmbed($string)
     {
     	$cutMe = 'watch?v=';
     	$orMe = 'youtu.be';
@@ -19,3 +19,43 @@ class Regex
 // echo $link->linkToEmbed("https://youtu.be/3QrQdv_TP5Q") . '<br />';
 // https://www.youtube.com/embed/3QrQdv_TP5Q
 
+/*
+README.md 
+
+composer require twig/extensions
+
+{{ tutorial.link|embed(tutorial.link) }}
+
+
+// src/Twig/AppExtension.php
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return array(
+            new TwigFilter('embed', array($this, 'embedFilter')),
+        );
+    }
+
+    public function embedFilter($string)
+    {
+        $cutMe = 'watch?v=';
+    	$orMe = 'youtu.be';
+    	$newString = str_replace($cutMe, "embed/", $string); 	
+    	$resultString = str_replace($orMe, "www.youtube.com/embed", $newString);
+    	return $resultString;
+    }
+}
+
+# config/services.yaml
+services:
+    App\Twig\AppExtension:
+        public: false
+        tags: ['twig.extension']
+
+*/
